e1420d4295f0a2a48230bd8e444adbe7ad2a1c62
lukehayes <lukepurplehayes@gmail.com>
2020-09-10 18:06:27 +0100

    Added title and content rows to create_posts_table migration

--- database/migrations/2020_09_10_152808_create_posts_table.php
+++ database/migrations/2020_09_10_152808_create_posts_table.php
@@ -16,6 +16,8 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title', 100);
+            $table->text('content');
         });
     }
 
