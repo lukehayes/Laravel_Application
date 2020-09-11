c32b079b17e8c16bf8e4a6cd8f922b7f8b9131aa
lukehayes <lukepurplehayes@gmail.com>
2020-09-11 09:27:56 +0100

    Added missing namespaces to PostSeeder works

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
@@ -13,7 +15,7 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('post')->insert([
+        DB::table('posts')->insert([
             'title' => Str::random(10),
             'content' => Str::random(50)
         ]);
