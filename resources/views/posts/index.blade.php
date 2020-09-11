288d801589cfd62af971d912775e0ca55045e604
lukehayes <lukepurplehayes@gmail.com>
2020-09-11 09:38:16 +0100

    Added basic output for all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,18 @@
 @extends('app')
 
 @section('main')
-<p>Index Post Content...</p>
+<p>All Posts:</p>
+
+<table>
+    
+</table>
+
+@foreach($posts as $post)
+    <div>
+        <h3>{{ $post->title }}</h3>
+        <p>{{ $post->content }}</p>
+    </div>
+@endforeach
+
+
 @endsection('main')
