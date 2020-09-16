14a62f496f9857be426955a340b2463fd2e512d3
lukehayes <lukepurplehayes@gmail.com>
2020-09-16 18:00:36 +0100

    Changed class string to Category::class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -31,6 +31,6 @@ class Post extends Model
      */
     public function category()
     {
-        return $this->belongsTo('App\Models\Category');
+        return $this->belongsTo(Category::class);
     }
 }
