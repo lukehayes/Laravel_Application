e8c814c4be236571800c43b7af8ff30efbc2081f
lukehayes <lukepurplehayes@gmail.com>
2020-09-16 08:59:13 +0100

    Replaced Bootstrap library with Bulma

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
         <title>Application</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <!-- Bulma -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.0/css/bulma.min.css" >
+
 
         <!-- Styles -->
         <style>
@@ -18,6 +20,7 @@
                 font-family: 'Nunito';
             }
         </style>
+
     </head>
 
 
