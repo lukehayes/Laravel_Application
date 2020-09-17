517f9a984fce956c6b1622eb0a501d403fdfdf8c
lukehayes <lukepurplehayes@gmail.com>
2020-09-17 16:33:23 +0100

    PostController::index how shows posts in reverse order

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::all()->reverse();
         return view('posts.index', ['posts' => $posts]);
     }
 
