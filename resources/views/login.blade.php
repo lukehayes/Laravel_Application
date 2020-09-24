de77861c4d10397cb2e80d9452fe62e758460792
lukehayes <lukepurplehayes@gmail.com>
2020-09-24 09:41:31 +0100

    Added error output to login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 
+@if($errors->any())
+<div class="section">
+    <div class="columns">
+        @foreach ($errors->all() as $error)
+        <div class="column is-full">
+            <div class="message is-danger">{{ $error }}</div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endif
+
 @section('login')
 <div class="section">
     <div class="container">
@@ -12,16 +24,16 @@
 
                     <div class="field">
                         <label class="label" for="">Email Address:</label>
-                            <div class="control">
-                                <input type="text" name="email" class="input">
-                            </div>
+                        <div class="control">
+                            <input type="text" name="email" class="input">
+                        </div>
                     </div>
 
                     <div class="field">
                         <label class="label" for="">Password:</label>
-                            <div class="control">
-                                <input type="password" name="password" class="input">
-                            </div>
+                        <div class="control">
+                            <input type="password" name="password" class="input">
+                        </div>
                     </div>
 
                     <button type="submit" class="button is-outline is-dark">Login</button>
