48225bf1e626316c3dd2cf886bc338b826b22e09
lukehayes <lukepurplehayes@gmail.com>
2020-09-24 09:26:54 +0100

    Added DashboardController::index()

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * The home view for the dashboard that shows once the 
+     * user has logged in.
+     */
+    public function index()
+    {
+        return view('dashoard.index');
+    }
 }
