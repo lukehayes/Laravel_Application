54794c75d33064b76e9bee01a1e4d7bdec5c2f81
lukehayes <lukepurplehayes@gmail.com>
2020-09-24 09:42:09 +0100

    Dashboard::index() now loads index.blade.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -12,6 +13,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashoard.index');
+        if(Auth::check())
+        {
+            return view('dashboard.index');
+        }else {
+            return view('login');
+        }
     }
 }
